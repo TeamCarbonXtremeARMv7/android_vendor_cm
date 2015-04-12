f320350af688223ba2c6373f2fdad44b647b60ba
ROM-PacMe <rafy.pachecomendez@gmail.com>
2015-04-12 18:57:47 -0300

    First Commit for Vendor CM LP 5.1

---
+++ config/common_full.mk
@@ -0,0 +1,28 @@
+# Inherit common CM stuff
+$(call inherit-product, vendor/cm/config/common.mk)
+
+# Include CM audio files
+include vendor/cm/config/cm_audio.mk
+
+# Include CM LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
+
+# Optional CM packages
+PRODUCT_PACKAGES += \
+    Galaxy4 \
+    HoloSpiralWallpaper \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    NoiseField \
+    PhaseBeam \
+    VisualizationWallpapers \
+    PhotoTable \
+    SoundRecorder \
+    PhotoPhase
+
+# Extra tools in CM
+PRODUCT_PACKAGES += \
+    vim \
+    zip \
+    unrar
